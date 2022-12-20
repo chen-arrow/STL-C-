41100280462bc4ee6e8431a58d12a4dce8c04265
arrow <2638023977@qq.com>
2022-12-20 10:02:04 +0800

    插入和删除

---
+++ STL的学习/插入和删除.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void printfVector(vector<int>& v)
+{
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+void test01()
+{
+	vector<int>v1;
+	v1.push_back(10);
+	v1.push_back(20);
+	v1.push_back(30);
+	v1.push_back(40);
+	v1.push_back(50);
+
+	//遍历
+	printfVector(v1);
+	//尾删除
+	v1.pop_back();
+	printfVector(v1);
+
+	//插入 第一个参数是迭代器
+	v1.insert(v1.begin(), 100);
+	printfVector(v1);
+
+	v1.insert(v1.begin(), 2, 1000);
+	printfVector(v1);
+
+	//删除 第一个参数是迭代器
+	v1.erase(v1.begin());
+	printfVector(v1);
+	//清空
+	//v1.erase(v1.begin(), v1.end());
+	v1.clear();
+	printfVector(v1);
+}
+int main()
+{
+	test01();
+}
