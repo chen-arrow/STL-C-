4e1b348980370c8309bd2d0f628f4ccb911e8641
arrow <2638023977@qq.com>
2022-12-30 16:34:23 +0800

    大小和交换

--- map的用法/大小和交换.cpp
+++ map的用法/大小和交换.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 #include<map>
-
+void printMap(map<int, int>m)
+{
+	for (map<int, int>::iterator it = m.begin(); it != m.end(); it++)
+	{
+		cout << "key= " << (*it).first << " value= " << (*it).second << endl;
+	}
+	cout << endl;
+}
 void test01()
 {
 	map<int, int>m;
@@ -9,8 +16,40 @@ void test01()
 	m.insert(pair<int, int>(3, 30));
 	m.insert(pair<int, int>(2, 20));
 	m.insert(pair<int, int>(4, 40));
+	if (m.empty())
+	{
+		cout << "m为空" << endl;
+	}
+	else
+	{
+		cout << "不为空" << endl;
+		cout << "大小为: " << m.size() << endl;
+	}
+	
+
+}
+//交换
+void test02()
+{
+	map<int, int>m1;
+	m1.insert(pair<int, int>(1, 10));
+	m1.insert(pair<int, int>(3, 30));
+	m1.insert(pair<int, int>(2, 20));
+	m1.insert(pair<int, int>(4, 40));
+
+	map<int, int>m;
+	m.insert(pair<int, int>(5, 100));
+	m.insert(pair<int, int>(6, 300));
+	m.insert(pair<int, int>(7, 200));
+	m.insert(pair<int, int>(8, 400));
+
+
+	m1.swap(m);
+	printMap(m1);
+	printMap(m);
 }
 int main()
 {
-	test01();
+	//test01();
+	test02();
 }
